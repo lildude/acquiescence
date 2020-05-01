ac02c85b07d68589f082117ecc4b65d19a16a240
Colin Seymour <lildood@gmail.com>
2020-05-01 14:45:14 +0100

    Try this

--- app.rb
+++ app.rb
@@ -70,6 +70,14 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+
+  def csrf_token
+    Rack::Csrf.csrf_token(env)
+  end
+
+  def csrf_tag
+    Rack::Csrf.csrf_tag(env)
+  end
 end
 
 get '/' do
